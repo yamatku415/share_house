4c46a32456d80260fe455eb433ce32f631a2ad7b
yamatku415 <87381371+yamatku415@users.noreply.github.com>
2022-10-05 21:22:33 +0900

    'カレンダー実装'

---
+++ lib/pages/calender.dart
@@ -0,0 +1,77 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalenderPage extends StatefulWidget {
+  const CalenderPage({Key? key}) : super(key: key);
+
+  @override
+  _CalenderPageState createState() => _CalenderPageState();
+}
+
+class _CalenderPageState extends State<CalenderPage> {
+  Map<DateTime, List> _eventsList = {};
+
+  DateTime _focused = DateTime.now();
+  DateTime? _selected;
+
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selected = _focused;
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Test A', 'Test B'],
+      DateTime.now(): ['Test C', 'Test D', 'Test E', 'Test F'],
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List getEvent(DateTime day) {
+      return _events[day] ?? [];
+    }
+
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2022, 4, 1),
+            lastDay: DateTime.utc(2025, 12, 31),
+            eventLoader: getEvent, //追記
+            selectedDayPredicate: (day) {
+              return isSameDay(_selected, day);
+            },
+            onDaySelected: (selected, focused) {
+              if (!isSameDay(_selected, selected)) {
+                setState(() {
+                  _selected = selected;
+                  _focused = focused;
+                });
+              }
+            },
+            focusedDay: _focused,
+          ),
+          //--追記--------------------------------------------------------------
+          ListView(
+            shrinkWrap: true,
+            children: getEvent(_selected!)
+                .map((event) => ListTile(
+                      title: Text(event.toString()),
+                    ))
+                .toList(),
+          )
+          //--------------------------------------------------------------------
+        ]));
+  }
+}
